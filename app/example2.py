02523bf016520330793a1fee4be775311b1cc91f
DEVILOCHEEK <123664311+DEVILOCHEEK@users.noreply.github.com>
2025-05-22 18:55:58 +0300

    Create example2.py

---
+++ app/example2.py
@@ -0,0 +1,15 @@
+def add(a, b):
+    """Повертає суму двох чисел."""
+    return a + b
+
+
+def multiply(a, b):
+    """Повертає добуток двох чисел."""
+    return a * b
+
+
+if __name__ == "__main__":
+    x = 5
+    y = 10
+    print("Сума:", add(x, y))
+    print("Добуток:", multiply(x, y))
